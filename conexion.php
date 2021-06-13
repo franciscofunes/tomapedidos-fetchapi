4dbb337cf9742f7ff688432877cbb0cd3a4d67c5
franciscofunes <ffunes90@gmail.com>
2021-06-13 19:14:41 -0300

    remotehost sql

--- conexion.php
+++ conexion.php
@@ -1,7 +1,12 @@
 <?php 
-    $servidor = "mysql:dbname=pedidos-lapaz;host=localhost";
-    $user = "root";
-    $pass="";
+    //localhost
+    //$servidor = "mysql:dbname=pedidos-lapaz;host=localhost";
+    //$user = "root";
+    //$pass="";
+    //remotehost
+    $servidor = "mysql:dbname=8qSPKXXNPP;host=remotemysql.com";
+    $user = "8qSPKXXNPP";
+    $pass="4wJzwBpTny";
 
     try {
         $pdo = new PDO($servidor,$user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"));
