bd78659d0af224a5e61475c08c98fe8ccc4b27d8
franciscofunes <ffunes90@gmail.com>
2021-06-13 19:18:05 -0300

    sacar contraseñas

--- conexion.php
+++ conexion.php
@@ -1,12 +1,9 @@
 <?php 
     //localhost
-    //$servidor = "mysql:dbname=pedidos-lapaz;host=localhost";
-    //$user = "root";
-    //$pass="";
-    //remotehost
-    $servidor = "mysql:dbname=8qSPKXXNPP;host=remotemysql.com";
-    $user = "8qSPKXXNPP";
-    $pass="4wJzwBpTny";
+    $servidor = "mysql:dbname=pedidos-lapaz;host=localhost";
+    $user = "root";
+    $pass="";
+
 
     try {
         $pdo = new PDO($servidor,$user, $pass, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"));
